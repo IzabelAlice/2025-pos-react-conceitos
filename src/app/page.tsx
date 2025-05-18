5dff3453326d3bf7d207352363b8e3dbdf170b5f
Izabel <izabel.dantas@escolar.ifrn.edu.br>
2025-05-18 20:08:42 -0300

    pratica 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@
 
 import React from "react";
 
-const Titulo = () => <h1 className="text-2xl font-bold mb-1">React - Conceitos básicos</h1>;
+const Titulo = () => (
+	<h1 className="text-2xl font-bold mb-1">React - Conceitos básicos</h1>
+);
 
-const SubTitulo = () => <h2 className="text-4xl font-bold mb-6">Lista de tarefas</h2>;
+const SubTitulo = () => (
+	<h2 className="text-4xl font-bold mb-6">Lista de tarefas</h2>
+);
 
 function Cabecalho() {
 	return (
@@ -15,24 +19,42 @@ function Cabecalho() {
 	);
 }
 
-class Tarefa extends React.Component {
-	render(): React.ReactNode {
-		return (
-			<div className="p-3 mb-3 rounded-lg shadow-md bg-gray-400">
-				<h3 className="text-xl font-bold">terminar repositorios durante 2025</h3>
-				<p className="text-sm">Pendente</p>
-			</div>
-		);
-	}
+interface TarefaProps {
+  titulo: string;
+	concluido?: boolean;
 }
-const Home = () => {
+
+const Tarefa: React.FC<TarefaProps> = ({ titulo, concluido }) => {
+	const classe = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
+		concluido
+			? "bg-gray-800 hover:border-gray-800"
+			: "bg-gray-400 hover:border-gray-400"
+	}`;
+
 	return (
-		<div className="container mx-auto p-4">
-			<Cabecalho />
-			<Tarefa />
+		<div
+			className={classe}
+			onClick={() => console.log(`A tarefa '${titulo}' foi clicada!`)}
+		>
+			<h3 className="text-xl font-bold">{titulo}</h3>
+			<p className="text-sm">{concluido ? "Concluída" : "Pendente"}</p>
 		</div>
 	);
 };
 
-export default Home;
+const Home = () => {
+	const tarefas = [
+		{ id: 1, title: "delectus aut autem", completed: false },
+		{ id: 2, title: "quis ut nam facilis et officia qui", completed: true },
+		{ id: 3, title: "fugiat veniam minus", completed: false },
+	];
 
+	return (
+		<div className="container mx-auto p-4">
+			<Cabecalho />
+			<Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed} />
+			<Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed} />
+		</div>
+	);
+}
+export default Home;
