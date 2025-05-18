99f40017eb31c32d8f3f4c9d8e49dea7481fb76b
Izabel <izabel.dantas@escolar.ifrn.edu.br>
2025-05-18 20:32:37 -0300

    pratica 8 finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 const Titulo = () => (
 	<h1 className="text-2xl font-bold mb-1">React - Conceitos básicos</h1>
@@ -20,24 +20,52 @@ function Cabecalho() {
 }
 
 interface TarefaProps {
-  titulo: string;
+	titulo: string;
 	concluido?: boolean;
 }
 
 const Tarefa: React.FC<TarefaProps> = ({ titulo, concluido }) => {
+	const [estaConcluido, setEstaConcluido] = useState(concluido);
+
 	const classe = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
-		concluido
+		estaConcluido
 			? "bg-gray-800 hover:border-gray-800"
 			: "bg-gray-400 hover:border-gray-400"
 	}`;
 
+	const escutarClique = () => {
+		console.log(`A tarefa '${titulo}' foi clicada!`);
+		setEstaConcluido(!estaConcluido);
+	};
+
 	return (
-		<div
-			className={classe}
-			onClick={() => console.log(`A tarefa '${titulo}' foi clicada!`)}
-		>
+		<div className={classe} onClick={() => escutarClique()}>
 			<h3 className="text-xl font-bold">{titulo}</h3>
-			<p className="text-sm">{concluido ? "Concluída" : "Pendente"}</p>
+			<p className="text-sm">{estaConcluido ? "Concluída" : "Pendente"}</p>
+		</div>
+	);
+};
+
+interface TarefaInterface {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+interface TareafasProps {
+	dados: Array<TarefaInterface>;
+}
+
+const Tarefas: React.FC<TareafasProps> = ({ dados }) => {
+	return (
+		<div>
+			{dados.map((tarefa) => (
+				<Tarefa
+					key={tarefa.id}
+					titulo={tarefa.title}
+					concluido={tarefa.completed}
+				/>
+			))}
 		</div>
 	);
 };
@@ -52,9 +80,9 @@ const Home = () => {
 	return (
 		<div className="container mx-auto p-4">
 			<Cabecalho />
-			<Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed} />
-			<Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed} />
+			<Tarefas dados={tarefas} />
 		</div>
 	);
-}
+};
+
 export default Home;
